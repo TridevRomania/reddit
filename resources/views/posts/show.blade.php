1fe6226b994eb93a4a7804f70ce5da2080254405
Adrian Mincu <mal13adi03@gmail.com>
2024-04-12 22:09:25 +0300

    feat: done homework

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -12,6 +12,14 @@
     <a href="{{ url()->previous() }}">back</a>
     <h1> {{ $post->name }} </h1>
     <p> {{ $post->content }} </p>
+    <a href="{{ route('posts.edit', $post) }}">Update Post</a>
+
+    <form action="{{ route('posts.destroy', $post) }}" method="POST">
+        @csrf
+        @method('DELETE')
+
+        <button type="submit">Delete</button>
+    </form>
 </body>
 
 </html>
